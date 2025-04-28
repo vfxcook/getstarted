5994d572bc313c9d3b4add096a7c417ed6518f2c
vfxcook <139741329+vfxcook@users.noreply.github.com>
2025-04-28 12:27:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/Website/', 
-}))
+  base: '/getstarted/', // <-- ADD THIS LINE exactly
+})
